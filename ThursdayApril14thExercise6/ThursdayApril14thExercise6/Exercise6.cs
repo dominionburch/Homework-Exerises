36b357fd9a72f5c86b910a36755f62ba048b2690
dominion <burch.d89@gmail.com>
2022-04-16 18:04:56 -0400

    Exercise 6 first attempt

---
+++ ThursdayApril14thExercise6/ThursdayApril14thExercise6/Exercise6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ThursdayApril14thExercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable for user input
+            char choice;
+
+            //Loop to print out Hello World and ask user wether they would like to continue
+            do
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine();
+                Console.Write("Would you like to continue (y/n)?: ");
+                choice = Convert.ToChar(Console.ReadLine());
+            } while (choice == 'y' || choice == 'Y');
+
+            //Exiting program
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
